8b3d6c3de9bcda905f168223e08af2ef9081aec7
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-10-19 03:02:49 -0300

    add collsion sound

--- src/main.rs
+++ src/main.rs
@@ -30,10 +30,18 @@ const GAP_BETWEEN_BRICKS: f32 = 5.0;
 const GAP_BETWEEN_BRICKS_AND_CEILING: f32 = 20.0;
 const GAP_BETWEEN_BRICKS_AND_SIDES: f32 = 20.0;
 
+//scoreboard
+const SCOREBOARD_FONT_SIZE: f32 = 40.0;
+const SCOREBOARD_TEXT_PADDING: Val = Val::Px(5.0);
+const TEXT_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
+const SCORE_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
+        .insert_resource(Scoreboard { score: 0 })
+        .add_systems(Update, (bevy::window::close_on_esc, update_scoreboard))
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Startup, setup)
         .add_systems(
@@ -72,9 +80,21 @@ struct WallBundle {
 #[derive(Component)]
 struct Brick;
 
+#[derive(Resource, Clone, Copy)]
+struct Scoreboard {
+    score: usize,
+}
+
+#[derive(Resource, Default, Deref, DerefMut)]
+struct CollisionSound(Handle<AudioSource>);
+
 fn setup(mut commands: Commands, assets_server: Res<AssetServer>) {
     //camera
     commands.spawn(Camera2dBundle::default());
+
+    //sound
+    let collision_sound = assets_server.load("sounds/breakout_collision.ogg");
+    commands.insert_resource(CollisionSound(collision_sound));
 
     //padle
     commands.spawn((
@@ -235,6 +255,29 @@ fn setup(mut commands: Commands, assets_server: Res<AssetServer>) {
             }
         }
     }
+
+    //Scoreboard
+    commands.spawn((TextBundle::from_sections([
+        TextSection::new(
+            "Score: ",
+            TextStyle {
+                font_size: SCOREBOARD_FONT_SIZE,
+                color: TEXT_COLOR,
+                ..default()
+            },
+        ),
+        TextSection::from_style(TextStyle {
+            font_size: SCOREBOARD_FONT_SIZE,
+            color: SCORE_COLOR,
+            ..default()
+        }),
+    ])
+    .with_style(Style {
+        position_type: PositionType::Absolute,
+        top: SCOREBOARD_TEXT_PADDING,
+        left: SCOREBOARD_TEXT_PADDING,
+        ..default()
+    }),));
 }
 
 fn move_paddle(
@@ -272,8 +315,8 @@ fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time_step: Res<
 
 fn check_ball_collisions(
     mut commands: Commands,
-    //mut score: ResMut<Scoreboard>,
-    //collision_sound: Res<CollisionSound>,
+    mut score: ResMut<Scoreboard>,
+    collision_sound: Res<CollisionSound>,
     mut ball_query: Query<(&mut Velocity, &Transform, &Ball)>,
     mut collider_query: Query<(Entity, &Transform, &Collider, Option<&Brick>)>, // Note the mutability for Brick
 ) {
@@ -305,8 +348,15 @@ fn check_ball_collisions(
                 }
 
                 if opt_brick.is_some() {
+                    score.score += 1;
                     commands.entity(other_entity).despawn()
                 }
+
+                //play sound
+                commands.spawn(AudioBundle {
+                    source: collision_sound.clone(),
+                    settings: PlaybackSettings::DESPAWN,
+                });
 
                 //if let Some(mut brick) = opt_brick {
                 //    score.score += 1;
@@ -316,13 +366,12 @@ fn check_ball_collisions(
                 //        commands.entity(other_entity).despawn(); // Despawn the Brick if health is 0 or less
                 //    }
                 //}
-
-                //play sound
-                // commands.spawn(AudioBundle {
-                //     source: collision_sound.clone(),
-                //     settings: PlaybackSettings::DESPAWN,
-                // });
             }
         }
     }
+}
+
+fn update_scoreboard(score: Res<Scoreboard>, mut query: Query<&mut Text>) {
+    let mut text = query.single_mut();
+    text.sections[1].value = score.score.to_string();
 }
